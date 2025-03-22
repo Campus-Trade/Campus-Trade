d12860fab9b5b0797f6a59c6c3a3fb58fa05900f
laylaamr <laylamuhana981@gmail.com>
2025-03-22 06:17:43 +0200

    firebase installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:campus_trade/campus_trade_app.dart';
 import 'package:flutter/material.dart';
-import 'campus_trade_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(
-    const CampusTradeApp(),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const CampusTradeApp());
 }
-
